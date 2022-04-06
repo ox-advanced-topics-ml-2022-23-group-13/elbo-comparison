651c654235ac3879e38e388cdb9322b62f921eb6
99StewartL <99StewartL@gmail.com>
2022-04-06 12:27:48 +0100

    dreg loss added

--- comparison/loss.py
+++ comparison/loss.py
@@ -92,6 +92,48 @@ def PIWAE(res: VAEForwardResult) -> tuple[torch.Tensor, torch.Tensor]:
     return miwae, iwae
 
 
+def DREG(res: VAEForwardResult) -> tuple[torch.Tensor, torch.Tensor]:
+    """
+        TODO:
+        Actually test this am sure I've got a dim wrong somewhere
+        Implement it in the same way as PIWAE on the training side
+    """
+
+    def log_prob(dist, vals):
+        log_probs = dist.log_prob(vals)
+        r = log_probs.sum(-1) 
+        return r  
+
+    ev = var_log_evidence(res)
+
+    """
+        stop_grad_log_q_z_x should only be a function of z and not of the distribution
+        but there is no dist.detach()
+        so this is a hack
+        it's gonna be slower but hopefull it works
+        an alternative would be to open up the model so we can detach mu and std directly
+        but this is a p big rewrite of a lot of this code
+    """
+    
+    lp_full = log_prob(res.post_dist, res.zs)
+    lp_partial = log_prob(res.post_dist, res.zs.detach())
+    stop_grad_log_q_z_x = lp_full + (lp_partial.detach() - lp_partial)
+
+    stop_grad_log_w = sum([
+          log_prob(res.prior_dist, res.zs),
+        + log_prob(res.lik_dist, res.xs),
+        - stop_grad_log_q_z_x
+        ])
+    
+
+    # these are a function of nothing
+    importance_weights = (stop_grad_log_w - torch.logsumexp(stop_grad_log_w,dim=0)).detach().exp()
+
+    infer_loss = - (importance_weights.pow(2) * stop_grad_log_w).sum(0).mean()
+
+    return ev, infer_loss
+
+
 
 def ELBO_loss(res: VAEForwardResult) -> torch.Tensor:
     return ELBO(res).mean(dim=BATCH_DIM)
